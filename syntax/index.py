b3b6cca73b13ef1bf02a8497b69ad222b271877c
gitPiyushhh <Piyushhhagarwal@gmail.com>
2022-02-13 16:33:29 +0530

    First commit

--- syntax/index.py
+++ syntax/index.py
@@ -15,86 +15,78 @@
 ########################################
 ############## Lists
 
-# nums = [1, 2, 3, 4, 5]
-# values = [2, 'Piyush', 1.5]
+nums = [1, 2, 3, 4, 5]
+values = [2, 'Piyush', 1.5]
 
 # # Lists in lists
-# comb = [nums, values]
+comb = [nums, values]
 
-# print(nums[0:4])
-# print(values)
-# print(comb)
+print(nums[0:4])
+print(values)
+print(comb)
 
 # # Methods 
 
 # # 1. Add an element
-# nums.append(6)
-# print(nums)
+nums.append(6)
+print(nums)
 
 # # 2. Remove an element
-# nums.remove(6)
-# print(nums)
+nums.remove(6)
+print(nums)
 
-# nums.pop(-1) #Remove the last element
-# print(nums)
+nums.pop(-1) #Remove the last element
+print(nums)
 
 ########################################
 ############## Touples {Immutable lists}
 
-# tup = (1, 2, 3, 4)
-# print(tup)
+tup = (1, 2, 3, 4)
+print(tup)
 
 
-# tup[1] = 0  # 🚫 Assignment not supported
+tup[1] = 0  # 🚫 Assignment not supported
 
 ########################################
 ############## Set {Uniqle values}
 
-# s = {1, 2, 1, 3, 2, 4}  # 1️⃣ Only 1 value
-# print(s)
+s = {1, 2, 1, 3, 2, 4}  # 1️⃣ Only 1 value
+print(s)
 
 ########################################
 ############## Dictionary {Like object literal}
 
-# data = {1: 'Piyush', 2: 'Jonas'}
+data = {1: 'Piyush', 2: 'Jonas'}
 
-# print(data)
-# print(data[2])
+print(data)
+print(data[2])
 
-# print(data.keys())  # To get all the keys
-# print(data.values()) # To get all the values
+print(data.keys())  # To get all the keys
+print(data.values()) # To get all the values
 
-# print(data.get(3, 'Not found'))  # Search the key if not found then print 'Not found'
+print(data.get(3, 'Not found'))  # Search the key if not found then print 'Not found'
 
-# keys = ['Piyush', 'Jonas']
-# values = ['python', 'JS']
+keys = ['Piyush', 'Jonas']
+values = ['python', 'JS']
 
-# pair = dict(zip(keys, values))
+pair = dict(zip(keys, values))
+print(pair)
 
-
-# print(pair)
 
 ## For finding the address of a variable
-# from xml.etree.ElementTree import PI
+a = 5
+b = 5
+print(id(a))  #If the values of the variable is same then they both will have the same addrrss
+print(id(b))
 
-
-# a = 5
-# b = 5
-# print(id(a))  #If the values of the variable is same then they both will have the same addrrss
-# print(id(b))
-
-# from numpy import var
-
-
-# print(type(a))
+print(type(a))
 
 #######################################
 ############# Type conversion
 
 
-# a = 5.5
-
-# print(int(a))
+a = 5.5
+print(int(a))
 
 
 #######################################
@@ -102,13 +94,13 @@
 
 # a = 25
 
-# print(bin(a))
-# print(oct(a))
-# print(hex(a))
+print(bin(a))
+print(oct(a))
+print(hex(a))
 
-# print(0b0101)  # Convert to the the decimal from binary
-# print(0o0101)  # Convert to the the decimal from octal
-# print(0x0101)  # Convert to the the decimal from hexa-decimal
+print(0b0101)  # Convert to the the decimal from binary
+print(0o0101)  # Convert to the the decimal from octal
+print(0x0101)  # Convert to the the decimal from hexa-decimal
 
 
 #######################################
@@ -119,4 +111,4 @@ b = 6
 
 b,a = a,b
 
-print(a,b) 
+print(a,b)
